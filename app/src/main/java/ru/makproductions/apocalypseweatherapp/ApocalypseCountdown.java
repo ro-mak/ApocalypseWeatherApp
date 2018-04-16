dbb4942949541025fd71501b4b870692d4542f7a
Inlinebrother <eee127@yandex.ru>
2018-04-16 06:13:29 +0300

    -added system time and inflated layout_countdown_view

--- app/src/main/java/ru/makproductions/apocalypseweatherapp/ApocalypseCountdown.java
+++ app/src/main/java/ru/makproductions/apocalypseweatherapp/ApocalypseCountdown.java
@@ -9,35 +9,44 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 public class ApocalypseCountdown extends Fragment {
-    private static final int MILLS_IN_SECOND = 3600;
+    private static final int SECONDS_IN_HOUR = 3600;
     private static final int SECONDS_IN_MUNUTE = 60;
     private static final int DELAY = 1000;
     private static final String KEY_SECONDS = "seconds";
     private static final String KEY_RUNNING = "running";
     private static final String KEY_WAS_RUNNING = "wasRunning";
-    private int seconds;
+    private long seconds;
     private boolean running;
     private boolean wasRunning;
+    private long currentTime;
+    private long dateOfDoom;
+    private long countDownTime;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.countdown_view,container,false);
+        currentTime = Calendar.getInstance().getTimeInMillis();
+        countDownTime = dateOfDoom - currentTime;
+        return rootView;
     }
 
-    private void runCountdown(View rootView, long dateOfDoom) {
+    private void runCountdown(View rootView) {
+        seconds = countDownTime;
         final TextView timeView = (TextView)
                 rootView.findViewById(R.id.countdown_view);
         final Handler handler = new Handler();
         handler.post(new Runnable() {
             @Override
             public void run() {
-                int hours = seconds / MILLS_IN_SECOND;
-                int minutes = (seconds % MILLS_IN_SECOND) / SECONDS_IN_MUNUTE;
-                int secs = seconds % 60;
+                long hours = seconds / SECONDS_IN_HOUR;
+                long minutes = (seconds % SECONDS_IN_HOUR) / SECONDS_IN_MUNUTE;
+                long secs = seconds % SECONDS_IN_MUNUTE;
                 String time = String.format(Locale.US, "%d:%02d:%02d", hours,
                         minutes, secs);
                 timeView.setText(time);
